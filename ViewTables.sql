c1fa7d06ac68735d2f2e8e74da03a958bf8b1f7b
bhartman4 <bh018420@ohio.edu>
2024-11-24 16:24:46 -0500

    created tables and view tables

--- ViewTables.sql
+++ ViewTables.sql
@@ -1,1 +1,36 @@
+-- View all records from the BasePizza table
+SELECT * FROM BasePizza;
+
+-- View all records from the Pizza table
+SELECT * FROM Pizza;
+
+-- View all records from the Toppings table
+SELECT * FROM Toppings;
+
+-- View all records from the Contains table
+SELECT * FROM Contains;
+
+-- View all records from the Discounts table
+SELECT * FROM Discounts;
+
+-- View all records from the Apply_Discount_Order table
+SELECT * FROM Apply_Discount_Order;
+
+-- View all records from the Apply_Discount_Pizza table
+SELECT * FROM Apply_Discount_Pizza;
+
+-- View all records from the Orders table
+SELECT * FROM Orders;
+
+-- View all records from the DineIn table
+SELECT * FROM DineIn;
+
+-- View all records from the Delivery table
+SELECT * FROM Delivery;
+
+-- View all records from the Pickup table
+SELECT * FROM Pickup;
+
+-- View all records from the Customer table
+SELECT * FROM Customer;
 
